f5a70176ea1cd41a30a94cc77c45fb0f825ed7a0
DilshaniN <dilshaninavodani@gmail.com>
2020-12-27 16:19:05 +0530

    Report Accident Activity Updated

---
+++ app/src/main/java/com/example/mrclaim/ReportAccident.java
@@ -0,0 +1,14 @@
+package com.example.mrclaim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportAccident extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_accident);
+    }
+}
