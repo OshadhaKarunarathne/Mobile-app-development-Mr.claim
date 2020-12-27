7015baa159703ea42fd90e193c5a3961d6695360
Praveen <palihena.praveen@gmail.com>
2020-12-27 23:49:00 +0530

    My accident activity and 119 call function added

--- app/src/main/java/com/example/mrclaim/MainActivity.java
+++ app/src/main/java/com/example/mrclaim/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +39,13 @@ public class MainActivity extends AppCompatActivity {
         for(int image: images) {
             flipperImages(image);
         }
-        //Navigation drawer
+        //Toolbar
             toolbar= findViewById(R.id.drawer_toolbar);
             setSupportActionBar(toolbar);
+
+            //Navigation drawer
             drawerLayout= findViewById(R.id.drawer);
             navigationView= findViewById(R.id.navigation_view);
-
             actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open, R.string.close);
             drawerLayout.addDrawerListener(actionBarDrawerToggle);
             actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
@@ -52,6 +57,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             actionBarDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_hamburg);
+
+            //119 button_
+            Button btn_emergency = (Button)findViewById(R.id.btn_emergency);
+            btn_emergency.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String number= "119";
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse("tel:"+number));
+                    startActivity(intent);
+                }
+            });
+
+            //button My Accident
+            button= (Button) findViewById(R.id.btn_accidents);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this,MyAccident.class);
+                    startActivity(intent);
+                }
+            });
     }
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater=getMenuInflater();
---
+++ app/src/main/java/com/example/mrclaim/MyAccident.java
@@ -0,0 +1,14 @@
+package com.example.mrclaim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyAccident extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_accident);
+    }
+}
