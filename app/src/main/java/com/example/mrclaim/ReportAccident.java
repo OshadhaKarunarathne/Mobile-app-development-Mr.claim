2c6bc0ce1cc6787b30d202a64aab087419d4de46
DilshaniN <dilshaninavodani@gmail.com>
2020-12-28 02:39:09 +0530

    Radio Buttons Toast Added

--- app/src/main/java/com/example/mrclaim/ReportAccident.java
+++ app/src/main/java/com/example/mrclaim/ReportAccident.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class ReportAccident extends AppCompatActivity {
 
@@ -13,5 +15,35 @@ public class ReportAccident extends AppCompatActivity {
         setContentView(R.layout.report_accident);
     }
 
-    public void onRadioButtonSelected(View view){}
+
+    public void onRadioButtonSelected(View view){
+
+        Boolean isChecked=((RadioButton)view).isChecked();
+
+        switch (view.getId())
+        {
+            case R.id.radioButton:
+                if (isChecked)
+                    showMessage("Mobility:Yes");
+                break;
+            case  R.id.radioButton2:
+                if (isChecked)
+                    showMessage("Mobility:No");
+        }
+        switch (view.getId())
+        {
+            case R.id.radioButton4:
+                if (isChecked)
+                    showMessage("Need Emergency Vehicle:Yes");
+                break;
+            case  R.id.radioButton3:
+                if (isChecked)
+                    showMessage("Need Emergency Vehicle:No");
+        }
+    }
+    public void showMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+
 }
