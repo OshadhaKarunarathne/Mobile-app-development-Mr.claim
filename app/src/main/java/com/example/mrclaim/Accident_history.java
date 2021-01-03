56948ea6296a13dcf31af9dc246fc0da168a91e1
oshadha <oshadhapiyumalaka@gmail.com>
2021-01-03 11:32:19 +0530

    accident history java file and xml started

---
+++ app/src/main/java/com/example/mrclaim/Accident_history.java
@@ -0,0 +1,14 @@
+package com.example.mrclaim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Accident_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident_history);
+    }
+}
