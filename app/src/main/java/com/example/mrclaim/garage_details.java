fc4d0ba1576828c417877b304fb09ac7194d9a0d
oshadha <oshadhapiyumalaka@gmail.com>
2021-01-05 11:12:46 +0530

    garage details updated

--- app/src/main/java/com/example/mrclaim/garage_details.java
+++ app/src/main/java/com/example/mrclaim/garage_details.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -18,8 +19,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -30,100 +34,48 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class garage_details extends AppCompatActivity {
+public class garage_details extends AppCompatActivity implements LocationListener{
 
-   /*         EditText Vnumber;
-            EditText gname;
-            EditText time;
-            EditText city;
-            EditText state;
-            EditText fulladdress;
-            Button send;
-            Button back;
-            long maxid=0;
+    Button btnLocation;
+    EditText Vehicle_No;
+    EditText gname;
+    EditText timedate;
+    EditText FullAddress;
+    Button showLocation;
     LocationManager locationManager;
     private double latitude;
     private double longitude;
 
 
-    DatabaseReference reference;
-    Report report;
-
-
-*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.report_accident);
-/*
-        Vnumber = findViewById(R.id.vehicle_no);
-        gname = findViewById(R.id.gname);
-        time = findViewById(R.id.timeanddate);
-        city = findViewById(R.id.city);
-        state = findViewById(R.id.state);
-        fulladdress=findViewById(R.id.FullAddress);
-        send=(Button) findViewById(R.id.SendData);
-        back = findViewById(R.id.button_back);
+        setContentView(R.layout.activity_garage_details);
 
-        reference=FirebaseDatabase.getInstance().getReference().child("Report");
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists())
-                    maxid=(snapshot.getChildrenCount());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-
-        report=new Report();
-
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String  Vnumber_create=Vnumber.getText().toString();
-                String  gname_create=gname.getText().toString();
-                String  time_create=time.getText().toString();
-                String  city_create=city.getText().toString();
-                String  state_create=state.getText().toString();
-                String  fulladdress_create=fulladdress.getText().toString();
-
-                reference.child(String.valueOf(maxid+1)).setValue(report);
-                Toast.makeText(garage_details.this, "Data Inserted Successfully!", Toast.LENGTH_SHORT).show();
+        btnLocation=findViewById(R.id.btnLocation);
+        Vehicle_No=(EditText)findViewById(R.id.Vehicle_No);
+        gname= findViewById(R.id.gname);
+        timedate=findViewById(R.id.timedate);
+        FullAddress=findViewById(R.id.FullAddress);
+        showLocation=findViewById(R.id.showLocation);
 
 
 
-                if (TextUtils.isEmpty((CharSequence) Vnumber_create)) {
-                    Vnumber.setError("This can not be Empty!");
-                    return;
-                }
-                if (TextUtils.isEmpty((CharSequence) gname_create)) {
-                    gname.setError("This can not be Empty!");
-                    return;
-                }
-            }
-        });
-
-        EditText editText = findViewById(R.id.timeanddate);
+        EditText editText = findViewById(R.id.timedate);
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy 'at' HH:mm:ss ");
         String currentDateandTime = sdf.format(new Date());
         editText.setText(currentDateandTime);
 
-        initViews();
+        intView();
 
-        send.setOnClickListener(new View.OnClickListener() {
+        btnLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if (ContextCompat.checkSelfPermission(garage_details.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(garage_details.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
@@ -132,51 +84,9 @@ public class garage_details extends AppCompatActivity {
                 }
             }
 
-            private void detectCurrentLocation() {
-            }
-        });
+        } );
+}
 
-
-    }
-
-
-
-    private void initViews() {
-
-        state = findViewById(R.id.state);
-        city = findViewById(R.id.city);
-        fulladdress = findViewById(R.id.FullAddress);
-        send = findViewById(R.id.SendData);
-    }
-
-    public void onLocationChanged(@NonNull Location location) {
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        findAddress();
-    }
-
-    private void findAddress() {
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(this, Locale.getDefault());
-
-        try {
-            addresses = geocoder.getFromLocation(latitude, longitude, 1);
-
-
-            String State = addresses.get(0).getAdminArea();
-            String City = addresses.get(0).getLocality();
-            String FullAddress = addresses.get(0).getAddressLine(0);
-
-
-
-            state.setText(State);
-            city.setText(City);
-            fulladdress.setText(FullAddress);
-        } catch (Exception e) {
-            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
     private void detectCurrentLocation() {
         Toast.makeText(this, "Getting your Current Location", Toast.LENGTH_SHORT).show();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -190,14 +100,63 @@ public class garage_details extends AppCompatActivity {
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
 
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+    }
+
+
+    private void intView() {
+        FullAddress = findViewById(R.id.FullAddress);
+        btnLocation = findViewById(R.id.btnLocation);
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        findAddress();
+
+    }
+
+    private void findAddress() {
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+                    String fullAddress = addresses.get(0).getAddressLine(0);
+
+
+
+            FullAddress.setText(fullAddress);
+        } catch (Exception e) {
+            Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
 
 
     @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        Toast.makeText(this, "Please Turn On Location.", Toast.LENGTH_SHORT).show();
+
+    }
+
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode==1){
             if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
@@ -208,6 +167,6 @@ public class garage_details extends AppCompatActivity {
             {
                 Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
             }
-        }       */
+        }
     }
 }
