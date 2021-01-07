6866dd58790d04a5c6f07fd99a8da9e27f0d22e4
oshadha <oshadhapiyumalaka@gmail.com>
2021-01-07 12:00:10 +0530

    Accident_history.java code updated

--- app/src/main/java/com/example/mrclaim/Accident_history.java
+++ app/src/main/java/com/example/mrclaim/Accident_history.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +37,8 @@ public class Accident_history extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference dbref;
 
+
+    long maxid=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,34 +52,86 @@ public class Accident_history extends AppCompatActivity {
         myListView=(ListView) findViewById(R.id.myListView);
         database=FirebaseDatabase.getInstance();
         dbref=database.getReference("Report");
+
+
+
         list=new ArrayList<>();
         adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.tvVno,list);
+
+
         dbref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                for (DataSnapshot ds:snapshot.getChildren())
-                {
-
-                    history=ds.getValue(History.class);
-                    list.add(history.getVehicleNo().toString()+"    -    "+history.getDateTime().toString());
-                }
-                myListView.setAdapter(adapter);
-
-
+                if (snapshot.exists())
+                    maxid=(snapshot.getChildrenCount());
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-
         });
+
+
+//database
+        dbref.orderByChild("currentUID").equalTo(FirebaseAuth.getInstance().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds:snapshot.getChildren())
+                {
+
+                   // Log.e("error",history.getDateTime());
+//                    Toast.makeText(Accident_history.this,history.getVehicleNo().toString(),Toast.LENGTH_SHORT).show();
+                    history=ds.getValue(History.class);
+                    list.add(history.getVehicleNo().toString()+"    -    "+history.getDateTime().toString());
+                }
+
+                myListView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+//
+//        dbref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                for (DataSnapshot ds:snapshot.getChildren())
+//                {
+//
+//                    history=ds.getValue(History.class);
+//                    list.add(history.getVehicleNo().toString()+"    -    "+history.getDateTime().toString());
+//                }
+//                myListView.setAdapter(adapter);
+//
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//
+//        });
 
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+
+                Toast.makeText(Accident_history.this,myListView.getItemAtPosition(position)+"",Toast.LENGTH_LONG).show();
+
                 Intent intent = new Intent(Accident_history.this,garage_details.class);
+
+                intent.putExtra("data", myListView.getItemAtPosition(position)+"");
+
+
                 startActivity(intent);
             }
         });
