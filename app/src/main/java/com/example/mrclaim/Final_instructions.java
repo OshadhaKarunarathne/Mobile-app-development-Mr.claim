07a152e50ba45f58c4eea32981e737ec20487169
oshadha <oshadhapiyumalaka@gmail.com>
2021-01-14 19:23:49 +0530

    final instruction activity created

---
+++ app/src/main/java/com/example/mrclaim/Final_instructions.java
@@ -0,0 +1,14 @@
+package com.example.mrclaim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Final_instructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_instructions);
+    }
+}
